60483686acb41bc1398b51e812f508fcfe5472be
JUDY <s2018s05@e-mirim.hs.kr>
2020-05-12 23:49:55 +0900

    0508 실습일지_08 개미수열

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,31 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            String sequence = "1";
+            int n = 1;
+            while(n<=20)
+            {
+                Console.WriteLine(n + "번째 수열 : " + sequence);
+                char num = sequence[0];
+                int count = 0;
+                string temp = " ";
+                for (int i = 0; i < sequence.Length; i++)
+                {
+                    if (num == sequence[i])
+                        count++;
+                    else
+                    {
+                        temp += "" + num + count;
+                        count = 0;
+                        num = sequence[i];
+                        count++;
+                    }
+                    if (i + 1 == sequence.Length)
+                        temp += "" + num + count;
+                }
+                sequence = temp;
+                n++;
+            }
         }
     }
 }
